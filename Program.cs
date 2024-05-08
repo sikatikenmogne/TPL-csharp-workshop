0aa165b19ecde197e81e7f08af2b153b4a005487
Sikati Kenmogne Samuel <71256717+sikatikenmogne@users.noreply.github.com>
2024-05-08 22:41:35 +0100

    Set answer of Q7 — Task.Factory write a simple program that demonstrates the use of `Task.Factory`. This construct is used to start and manage tasks.

--- Program.cs
+++ Program.cs
@@ -1,81 +1,31 @@
 ﻿﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace test
 {
     class Program
     {
-        delegate void DELG();
-        delegate void EVT(object o);
-        static event EVT evt;
-        static int loop;
-        static object LOCK;
         static void Main(string[] args)
         {
-            DELG dlG_time = new DELG(time);
-            DELG dlg_multiCast;
-            IAsyncResult asyncR;
-            LOCK = new object();
-            loop = 0;
-            evt += new EVT(state_display);
-            var an_type = new {msg_pext = "pextension",msg_noext="noextension"};
-            System.Threading.Thread thd_timeInvok = 
-                new System.Threading.Thread(new System.Threading.ThreadStart(dlG_time.Invoke));
-            System.Threading.Thread thd_paraExt =
-                new System.Threading.Thread(
-                    new System.Threading.ThreadStart(() =>
-                    {
-                        System.Threading.Tasks.Parallel.For(0, 10, i =>
-                        {
-                            Console.WriteLine(@"{0}",an_type.msg_pext);
-                            System.Threading.Thread.Sleep(1000);
-                        });
-                        evt((object)an_type.msg_pext);
-                    }));
-            System.Threading.Thread thd_noParaExt = 
-                new System.Threading.Thread(
-                    new System.Threading.ThreadStart(()=>
-                    {
-                        for (int i = 0; i < 10; i++)
-                        {
-                            Console.WriteLine(@"{0}",an_type.msg_noext);
-                            System.Threading.Thread.Sleep(1000);
-                        };
-                        evt((object)an_type.msg_noext);
-                    }));
-            asyncR = dlG_time.BeginInvoke((async) =>
-                {
-                    DELG d = (DELG)((System.Runtime.Remoting.Messaging.AsyncResult)async).AsyncDelegate;
-                    d.EndInvoke(async);
-                    Console.Write("Fin des traveaux");
-                }, dlG_time);
-            dlg_multiCast = thd_paraExt.Start;
-            dlg_multiCast += thd_noParaExt.Start;
-            dlg_multiCast.Invoke();
-            while (!asyncR.IsCompleted) { Console.WriteLine("Travaux en cours..."); System.Threading.Thread.Sleep(5000); }
-            Console.Read();
-        }
-        static void time()
-        {
-            long t = 0;
-            lock (LOCK)
+            Task[] tasks = new Task[10];
+            const int UPPER = 10;
+
+            for (int i = 0; i < UPPER; i++)
             {
-                while (loop < 2)
-                {
-                    Console.WriteLine(t.ToString());
-                    t += 1;
-                    System.Threading.Thread.Sleep(1000);
-                }
-            }  
-        }
-        static void state_display(object o)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("{0} STOP", (string)o);
-            Console.ForegroundColor = ConsoleColor.Gray;
-            System.Threading.Interlocked.Increment(ref loop);
+                int val = i;
+                tasks[i] = Task.Factory.StartNew(() => {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Task number started {0}",val);
+                    
+                    System.Threading.Thread.Sleep(val*500);
+
+                    // Simulate some work by sleeping for 2 seconds
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Task number {0} completed.", val);
+                    Console.ResetColor();
+                } );
+            }
+            Task.WaitAll(tasks);
         }
     }
 }
